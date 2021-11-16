3e8b25b629623fff8ab004978702fbb323314a5a
Designer <85710656+Designer0001@users.noreply.github.com>
2021-11-15 19:51:46 -0500

    inital commit

---
+++ scripts/index.js
@@ -0,0 +1,38 @@
+$(document).ready(function(e){
+	$win = $(window);
+	$navbar = $("#header");
+	$toggle = $(".toggle-button");
+	var width = $navbar.width();
+
+	toggleOnClick($win, $navbar, width);
+
+	//resize event
+	$win.resize(function(){
+		toggleOnClick($win, $navbar, width);
+	});
+
+	$toggle.click(function(){
+		$navbar.toggleClass("toggle-left");
+	});
+
+});
+
+function toggleOnClick($win, $navbar, width) {
+	if ($win.width() <= 768) {
+		$navbar.css({left: `-${width}px`});
+	} else {
+		$navbar.css({left: '0px'});
+	}
+}
+
+var typed = new Typed('#typed', {
+	strings: [
+	    "Mercy-Crew",
+	    "ratting",
+	    "Designer#0001",
+		"known",
+	],
+	typeSpeed: 50,
+	backSpeed: 50,
+	loop: true
+});
---
+++ scripts/updates_data_file.js
@@ -0,0 +1,22 @@
+latest_updates = [
+	{
+		"content" : "COVIDMINDER - Data Visualization Tool Examines Community Factors Underlying COVID-19 Outcomes",
+		"url": "https://news.rpi.edu/content/2020/04/13/data-visualization-tool-examines-community-factors-underlying-covid-19-outcomes",
+		"date": "April 13, 2020"
+	},
+	{
+		"content" : "Paper published: \"Prediction of Gold Stock Market Using Hybrid Approach",
+		"url": "https://link.springer.com/chapter/10.1007/978-981-13-5802-9_70",
+		"date": "April 24, 2019"
+	}
+]
+
+publications = [
+	{
+		"content": "Prediction of Gold Stock Market Using Hybrid Approach",
+		"cite": "Vimuktha E. Salis, Animesh Singh, Akanksha Kumari \"Salis V.E., Kumari A., Singh A. (2019) Prediction of Gold Stock Market Using Hybrid Approach. In: Sridhar V., Padma M., Rao K. (eds) Emerging Research in Electronics, Computer Science and Technology. Lecture Notes in Electrical Engineering, vol 545. Springer, Singapore. https://doi.org/10.1007/978-981-13-5802-9_70",
+		"url": "https://link.springer.com/chapter/10.1007/978-981-13-5802-9_70",
+		"code": "",
+		"date": "April 24, 2019"
+	}
+]
